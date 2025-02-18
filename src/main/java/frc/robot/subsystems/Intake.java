b6340d1d0b96ad9a3ef36fc266b5ed1ee386a3c5
justfishin1999 <107942195+justfishin1999@users.noreply.github.com>
2025-02-18 11:44:36 -0600

    clean up variables

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,68 +1,58 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.NeutralOut;
-import com.ctre.phoenix6.controls.VelocityVoltage;
-import com.ctre.phoenix6.hardware.TalonFX;
-
 public class Intake extends SubsystemBase{
-    double s_kP, s_kI, s_kD, s_kS, s_kV, s_kA, s_Acceleration, s_CruiseVelo, s_Jerk, s_motionMagicA, s_motionMagicV;
-    int s_motorID;
     TalonFX s_IntakeMotor;
-    NeutralOut s_brake;
     VelocityVoltage m_velocityVoltage;
     DigitalInput m_photoEye;
     boolean photoCell;
 
     public Intake() {
-        s_kP = Constants.IntakeConstants.kP;
-        s_kI = Constants.IntakeConstants.kI;
-        s_kD = Constants.IntakeConstants.kD;
-        s_kS = Constants.IntakeConstants.kS;
-        s_kV = Constants.IntakeConstants.kV;
-        s_kA = Constants.IntakeConstants.kA;
-
-        s_motorID = Constants.IntakeConstants.motor1ID;
-
         m_velocityVoltage = new VelocityVoltage(0).withSlot(0);
 
-        s_IntakeMotor = new TalonFX(s_motorID);
+        s_IntakeMotor = new TalonFX(Constants.IntakeConstants.motor1ID);
 
-        m_photoEye = new DigitalInput(7);
+        m_photoEye = new DigitalInput(Constants.IntakeConstants.photoCellDIO);
 
         TalonFXConfiguration config = new TalonFXConfiguration();
-        config.Slot0.kP = s_kP;
-        config.Slot0.kI = s_kI;
-        config.Slot0.kD = s_kD;
-        config.Slot0.kS = s_kS;
-        config.Slot0.kV = s_kV;
-        config.Slot0.kA = s_kA;
+        config.Slot0.kP = Constants.IntakeConstants.kP;
+        config.Slot0.kI = Constants.IntakeConstants.kI;
+        config.Slot0.kD = Constants.IntakeConstants.kD;
+        config.Slot0.kS = Constants.IntakeConstants.kS;
+        config.Slot0.kV = Constants.IntakeConstants.kV;
+        config.Slot0.kA = Constants.IntakeConstants.kA;
 
         try{
             s_IntakeMotor.getConfigurator().apply(config);
-            System.out.println("Successfully configured intake motor!!");
+            System.out.println("!!Successfully configured intake motor!!");
         }
         catch(Exception e1){
-            DriverStation.reportWarning(getName(),e1.getStackTrace());
-            System.out.println("Failed to apply Intake motor configs: "+e1.getStackTrace());
+            DriverStation.reportWarning("Failed to apply intake motor configs",true);
+            System.out.println("Failed to apply Intake motor configs: "+e1.toString());
         }
 
     }
 
+    @Override
     public void periodic(){
         SmartDashboard.putNumber("Intake Velocity",s_IntakeMotor.getVelocity().getValueAsDouble());
 
+        //periodically get the status of the photo cell
         photoCell = m_photoEye.get();
         SmartDashboard.putBoolean("Intake Photo Cell Status",photoCell);
     }
 
     public boolean getPhotoCell(){
+        //return the value of the photo cell
         return photoCell;
     }
 
