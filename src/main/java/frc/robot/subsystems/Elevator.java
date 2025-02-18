2947465bc45fbb0f876d5ac041b7e674aa364593
justfishin1999 <107942195+justfishin1999@users.noreply.github.com>
2025-02-18 11:44:01 -0600

    clean up variables

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,77 +1,49 @@
 package frc.robot.subsystems;
 
-import java.util.function.BooleanSupplier;
-
-import com.ctre.phoenix6.CANBus;
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.configs.TalonFXConfigurator;
 import com.ctre.phoenix6.controls.Follower;
-import com.ctre.phoenix6.controls.MotionMagicExpoVoltage;
-import com.ctre.phoenix6.controls.MotionMagicTorqueCurrentFOC;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.controls.NeutralOut;
-import com.ctre.phoenix6.controls.PositionTorqueCurrentFOC;
-import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.GravityTypeValue;
 import com.ctre.phoenix6.signals.StaticFeedforwardSignValue;
-import com.revrobotics.spark.*;
 
-import static edu.wpi.first.units.Units.*;
-
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants;
 
 public class Elevator extends SubsystemBase{
-    double s_kP, s_kI, s_kD, s_kS, s_kV, s_kA, s_Acceleration, s_CruiseVelo, s_Jerk, s_motionMagicA, s_motionMagicV;
-    int s_topMotorID, s_bottomMotorID;
     TalonFX s_elevator1, s_elevator2;
     NeutralOut s_brake;
     MotionMagicVoltage s_request;
 
 
     public Elevator() {
-        s_kP = Constants.ElevatorConstants.kP;
-        s_kI = Constants.ElevatorConstants.kI;
-        s_kD = Constants.ElevatorConstants.kD;
-        s_kA = Constants.ElevatorConstants.kA;
-        s_Acceleration = Constants.ElevatorConstants.kAcceleration;
-        s_CruiseVelo = Constants.ElevatorConstants.kCruiseVelo;
-        s_Jerk = Constants.ElevatorConstants.kJerk;
-        s_topMotorID = Constants.ElevatorConstants.topMotorID;
-        s_bottomMotorID = Constants.ElevatorConstants.bottomMotorID; 
-        s_motionMagicA = Constants.ElevatorConstants.kMotionMagicA;
-        s_motionMagicV = Constants.ElevatorConstants.kMotionMagicV;
-
-        s_elevator1 = new TalonFX(s_topMotorID);
-        s_elevator2 = new TalonFX(s_bottomMotorID);
+        s_elevator1 = new TalonFX(Constants.ElevatorConstants.topMotorID);
+        s_elevator2 = new TalonFX(Constants.ElevatorConstants.bottomMotorID);
 
         TalonFXConfiguration config = new TalonFXConfiguration();
         CurrentLimitsConfigs limitConfigs = new CurrentLimitsConfigs();
 
         s_brake = new NeutralOut();
 
-        config.Slot0.kP = s_kP;
-        config.Slot0.kI = s_kI;
-        config.Slot0.kD = s_kD;
-        config.Slot0.kA = s_kA;
+        config.Slot0.kP = Constants.ElevatorConstants.kP;
+        config.Slot0.kI = Constants.ElevatorConstants.kI;
+        config.Slot0.kD = Constants.ElevatorConstants.kD;
+        config.Slot0.kA = Constants.ElevatorConstants.kA;
 
         config.Slot0.GravityType = GravityTypeValue.Elevator_Static;
         config.Slot0.StaticFeedforwardSign = StaticFeedforwardSignValue.UseVelocitySign;
 
-        config.MotionMagic.MotionMagicCruiseVelocity = s_CruiseVelo;
-        config.MotionMagic.MotionMagicAcceleration = s_Acceleration;
+        config.MotionMagic.MotionMagicCruiseVelocity = Constants.ElevatorConstants.kCruiseVelo;
+        config.MotionMagic.MotionMagicAcceleration = Constants.ElevatorConstants.kAcceleration;
 
 
         // enable stator current limit
-        limitConfigs.StatorCurrentLimit = 50; 
-        limitConfigs.StatorCurrentLimitEnable = true;
+        limitConfigs.StatorCurrentLimit = Constants.ElevatorConstants.currentLimit; 
+        limitConfigs.StatorCurrentLimitEnable = Constants.ElevatorConstants.enableCurrentLimits;
 
         //try to apply configuration to motors, throw warning to driver station if it doesn't work
         try{
@@ -81,9 +53,10 @@ public class Elevator extends SubsystemBase{
             s_elevator2.getConfigurator().apply(config.MotionMagic);
             s_elevator1.getConfigurator().apply(limitConfigs);
             s_elevator2.getConfigurator().apply(limitConfigs);
+            System.out.println("!!Successfully configured elevator motors!!");
         } catch(Exception e1) {
-            DriverStation.reportWarning(getName(), e1.getStackTrace());
-            System.out.println("Failed to apply elevator motor config: "+e1.getStackTrace());
+            DriverStation.reportWarning("Failed to apply elevator motor config",true);
+            System.out.println("Failed to apply elevator motor config: "+e1.toString());
         }
         //set boolean false to true to invert motor direction
         
@@ -94,9 +67,10 @@ public class Elevator extends SubsystemBase{
 
     public void setElevatorSetpoint(double elevatorSetpoint){
         s_elevator1.setControl(s_request.withPosition(elevatorSetpoint));
-        s_elevator2.setControl(new Follower(s_topMotorID,false));
+        s_elevator2.setControl(new Follower(Constants.ElevatorConstants.topMotorID,false));
     }
 
+    @Override
     public void periodic(){
         //periodically output the elevator position and speed
         SmartDashboard.putNumber("Elevator Position:",s_elevator1.getPosition().getValueAsDouble());
