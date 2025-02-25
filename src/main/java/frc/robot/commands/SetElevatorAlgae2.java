5198ec9224a0c3fb974bfe1faab9c3ab9ccfb667
justfishin1999 <107942195+justfishin1999@users.noreply.github.com>
2025-02-25 16:55:03 -0600

    Update SetElevatorAlgae2.java

--- src/main/java/frc/robot/commands/SetElevatorAlgae2.java
+++ src/main/java/frc/robot/commands/SetElevatorAlgae2.java
@@ -27,7 +27,7 @@ public class SetElevatorAlgae2 extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    s_elevator.setElevatorSetpoint(-30.9);
+    s_elevator.setElevatorSetpoint(-29.9);
   }
 
   // Called once the command ends or is interrupted.
